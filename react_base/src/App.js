4cb34c55e80a1cae68c1cfc5966e2fbc10cae1db
parkjongsu <jspark@tecace.com>
2022-10-05 15:49:54 +0900

    Updated: S3 upload  function

--- react_base/src/App.js
+++ react_base/src/App.js
@@ -1,7 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import AWS from "aws-sdk"
 
 function App() {
+  AWS.config.update({
+    region: "ap-northeast-2",
+    credentials: new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: "ap-northeast-2:6ee23d75-4e72-4315-a378-5548b22d70fc",
+    }),
+  })
+
+  const handleFileInput = e => {
+    const file = e.target.files[0]
+  
+    const upload = new AWS.S3.ManagedUpload({
+      params: {
+        Bucket: "yalu-s3",
+        Key: file.name,
+        Body: file,
+      },
+    })
+  
+    const promise = upload.promise()
+  
+    promise.then(
+      function (data) {
+        alert("이미지 업로드에 성공했습니다.")
+      },
+      function (err) {
+        return alert("오류가 발생했습니다: ", err.message)
+      }
+    )
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +48,8 @@ function App() {
         >
           Learn React
         </a>
+        <input type="file" id="upload" className="image-upload" onChange = {handleFileInput}/>
+        <label htmlFor="upload" className="image-upload-wrapper">S3 Upload</label>
       </header>
     </div>
   );
